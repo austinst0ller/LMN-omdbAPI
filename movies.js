bd10277f280a4c75f3c0a7feffedaa03bf9c6c9b
Austin <astoller66@gmail.com>
2024-01-01 19:42:49 -0600

    changes

--- movies.js
+++ movies.js
@@ -1,1 +1,35 @@
-console.log(fetch("https://www.omdbapi.com/?i=tt3896198&apikey=ba040381"))
+// console.log(fetch("https://www.omdbapi.com/?i=tt3896198&apikey=ba040381"))
+
+async function main() {
+
+  let keyword = 'fast';
+
+  const movieCardTemplate = document.querySelector("[movie-data-template]")
+
+  const response = await fetch(`https://www.omdbapi.com/?apikey=ba040381&s=${keyword}`);
+  const movieData = await response.json();
+  const movieListEl = document.querySelector(".movie__list");
+
+
+  const movies = movieData.Search
+
+
+  console.log(movieData)
+
+  // movieListEl.innerHTML = movies
+  //   .map(
+  //     (movies) =>
+  //       `<div class="movie__card">
+  //         <div class="movie__card--container">
+  //           <h3>${movies.title}</h3>
+  //           <p><b>Year:</b>${movies.year}</p>
+  //           <p><b>Rating:</b>xxxxx</p>
+  //           <p><b>Runtime:</b>xxxxx</p>
+  //           <p><b>Genre:</b>xxxxx</p>
+  //         </div>
+  //       </div>`
+  //   )
+  //   .join("");
+}
+
+main();
